4039afe3bf144a034e66c9ad6a5f59b34c652e9a
Tung Lam <tunglam123ik54@gmail.com>
2023-02-13 19:20:20 +0700

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import React from 'react'
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    React.createElement("div",null,)
+  );
+}
+
+export default App;
